8be4b17800df2fd710710f83643d7efb087f5b9f
Thomas Meier <thomas.meier5@uzh.ch>
2020-03-18 11:05:49 +0100

    corrected version check

--- EOSlib.c
+++ EOSlib.c
@@ -34,7 +34,7 @@ EOSMATERIAL *EOSinitMaterial(int iMat, double dKpcUnit, double dMsolUnit, const 
 	} else if (iMat>=iMATTILLOTSONMIN && iMat<=iMATTILLOTSONMAX)
 	{
 		/* Check if the Tillotson library has the right version. */
-		if (TILL_VERSION_MAJOR != 3 && TILL_VERSION_MINOR < 4) {
+		if (TILL_VERSION_MAJOR != 3 || TILL_VERSION_MINOR < 4) {
 			fprintf(stderr, "EOSinitMaterial: Tillotson library has the wrong version (%s)\n", TILL_VERSION_TEXT);
 			exit(1);
 		}
