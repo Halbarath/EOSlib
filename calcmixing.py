491f2bff3e143f8ede5d1774dc4f4b5338cc954a
Thomas Meier <meiers.thomas@bluewin.ch>
2023-11-30 11:31:22 +0100

    First steps for creating mixing tables

--- calcmixing.py
+++ calcmixing.py
@@ -1,45 +1,47 @@
 #!/usr/bin/env python3
 
-
-from __future__ import print_function
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 from scipy import optimize
 import numpy as np
 
 from subprocess import PIPE, Popen
-import sys
-import os
 
-
-def P_of_rhot(rho, T, input="aneos-dunite-collins-2014.input"):
-    cmd = "./calcPressureRhoTMANEOS {:} {:} {:}".format(rho, T, input)
-
+def EOSPofRhoT(iMat, rho, T):
+    cmd = "./callEOSlibforMixing {:} {:} {:}".format(iMat, rho, T)
     process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
     out = process.communicate()[0]
-
     P = float(out.split()[5])
     return P
 
-def rho_of_pt(P, T, input="aneos-dunite-collins-2014.input"):
-        func = lambda rho: P - P_of_rhot(rho, T, input)
+def EOSUofRhoT(iMat, rho, T):
+    cmd = "./callEOSlibforMixing {:} {:} {:}".format(iMat, rho, T)
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    out = process.communicate()[0]
+    U = float(out.split()[7])
+    return U
 
-        rho_min = 1e-25
-        rho_max = 1e3
+def EOSPUofRhoT(iMat, rho, T):
+    cmd = "./callEOSlibforMixing {:} {:} {:}".format(iMat, rho, T)
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    out = process.communicate()[0]
+    P = float(out.split()[5])
+    U = float(out.split()[7])
+    return P, U
 
-        sol = optimize.root_scalar(func, method='bisect', bracket=(rho_min, rho_max))
+def calcRhoOfPT(iMat, P, T, rho_min = 1e-4, rho_max = 1e3):
+    func = lambda rho: P - EOSPofRhoT(iMat, rho, T)
+    sol = optimize.root_scalar(func, method='bisect', bracket=(rho_min, rho_max))
+    return sol.root
 
-        return sol.root
+if __name__ == '__main__':
+    iMat = 52
+    rho = 5.0
+    T = 300.0
 
-rho = 19.0
-T= 1.0
-P = P_of_rhot(rho, T, input="aneos-iron-stewart-2020.input")
-
-print("rho= {:15.7E} T= {:15.7E} P= {:15.7E}".format(rho, T, P))
-
-rho_inv = rho_of_pt(P, T, input="aneos-iron-stewart-2020.input")
-
-
-print("rho= {:15.7E} T= {:15.7E} P= {:15.7E} rho_inv= {:15.7E}".format(rho, T, P, rho_inv))
-
-
+    rho_min = 1e-4
+    rho_max = 999.0
+    P = EOSPofRhoT(iMat, rho, T)
+    print(P)
+    rho_new = calcRhoOfPT(iMat, P, T, rho_min, rho_max)
+    print(rho_new)
+    u = EOSUofRhoT(iMat, rho_new, T)
+    print(u)
