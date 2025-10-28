5219e3e355086b9299fda6b3b6eb050cb1194ec2
Thomas Meier <meiers.thomas@bluewin.ch>
2025-10-28 13:58:47 +0100

    Added a tool to calculate mixing tables, might not work in all cases. The resulting table can be used as a REOS3 table, as it does not contain entropy.

---
+++ calcmixingparallel.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from scipy import optimize
+import numpy as np
+import sys
+from joblib import Parallel, delayed
+
+from subprocess import PIPE, Popen
+
+def EOSPofRhoT(iMat, rho, T):
+    cmd = "./callEOSlibforMixing {:} {:} {:}".format(iMat, rho, T)
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    out = process.communicate()[0]
+    P = float(out.split()[5])
+    return P
+
+def EOSUofRhoT(iMat, rho, T):
+    cmd = "./callEOSlibforMixing {:} {:} {:}".format(iMat, rho, T)
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    out = process.communicate()[0]
+    U = float(out.split()[7])
+    return U
+
+def EOSPUofRhoT(iMat, rho, T):
+    cmd = "./callEOSlibforMixing {:} {:} {:}".format(iMat, rho, T)
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    out = process.communicate()[0]
+    P = float(out.split()[5])
+    U = float(out.split()[7])
+    return P, U
+
+def calcRhoOfPT(iMat, P, T):
+    cmd = "./callEOSlibRhoofPT {:} {:} {:}".format(iMat, P, T)
+    process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    out = process.communicate()[0]
+    return float(out.split()[1])
+
+def calcRhoMixOfPT(iMatX, iMatY, X, Y, P, T):
+    rhoMixInv = X / calcRhoOfPT(iMatX,P,T) + Y / calcRhoOfPT(iMatY,P,T)
+    #print(1.0 / rhoMixInv)
+    return 1.0 / rhoMixInv
+
+def innerloopbody(rho,T, iMatX, iMatY, X, Y, P_min, P_max):
+    print("Calculating at rho = {:}, T = {:}".format(rho, T))
+    func = lambda P: rho - calcRhoMixOfPT(iMatX, iMatY, X, Y, P, T)
+    sol = optimize.root_scalar(func, method='brenth', bracket=(P_min, P_max))
+    P = sol.root
+    rhoX = calcRhoOfPT(iMatX, P, T)
+    rhoY = calcRhoOfPT(iMatY, P, T)
+    umix = X * EOSUofRhoT(iMatX, rhoX, T) + Y * EOSUofRhoT(iMatY, rhoY, T)
+    return rho, T, P, umix
+
+if __name__ == '__main__':
+    iMatX = int(sys.argv[1])
+    iMatY = int(sys.argv[2])
+    filename = sys.argv[3]
+    X = 0.18
+    Y = 1.0 - X
+    P_min = 1e5
+    P_max = 1e17
+
+    rho_axis = np.logspace(-1,2,151)
+    T_axis = np.logspace(2,5,151)
+
+    results = Parallel(n_jobs=32)(delayed(innerloopbody)(rho,T, iMatX, iMatY, X, Y, P_min, P_max) for T in T_axis for rho in rho_axis)
+    f = open(filename, "w")
+    for entry in results:
+        (rho, T, P, umix) = entry
+        f.write("{:} {:} {:} {:}\n".format(rho, T, P/1e10, umix/1e10))
+        f.flush()
+    f.close()
