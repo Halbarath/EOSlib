cddec1760b1268016ecb88a2a30f176c905b0fb4
Christian Reinhardt <christian.reinhardt@ics.uzh.ch>
2026-03-25 08:31:04 +0100

    Increase the range of material ids for the SCvH EOS.

--- EOSlib.h
+++ EOSlib.h
@@ -80,7 +80,7 @@
 #define MAT_REOS3_MIN 101
 #define MAT_REOS3_MAX 109
 #define MAT_SCVHEOS_MIN 110
-#define MAT_SCVHEOS_MAX 115
+#define MAT_SCVHEOS_MAX 130
 
 #define EOS_TRUE 1
 #define EOS_FALSE 0
